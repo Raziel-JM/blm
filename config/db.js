1eaf65ea1e3456ec68bebc05dbba49e398ea07cc
tu_nombre_de_usuario_en_github <tu_correo_electronico_asociado_a_github>
2024-01-27 00:47:44 -0600

    Actualización a la nueva versión

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL); 
+    console.log(`Connected To Mongodb Database ${mongoose.connection.host || "localhost"}`.bgBlue.white);
+  } catch (error) {
+    console.log(`Error de la base de datos Mongodb ${error}`.bgRed.white);
+  }
+};
+
+export default connectDB;
+
--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 import express from "express";
 import router from "./routes/testRoutes.js";
+import dotenv from "dotenv";
+import colors from "colors";
+import morgan from "morgan";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+// Configuración de dotenv
+dotenv.config();
+
+// Conectar a la base de datos
+connectDB();
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
+// Middleware para manejar datos JSON y habilitar CORS
+app.use(express.json());
+app.use(cors());
+
+// Middleware para el registro de solicitudes con morgan
+app.use(morgan("dev"));
+
+// Conectar a la base de datos
+try {
+  connectDB();
+} catch (error) {
+  console.error(colors.bgRed.white("Error connecting to MongoDB:"), error);
+  process.exit(1); // Salir del proceso con código de error
+}
 
 app.use("/api/v1/test", router);
 
 app.listen(PORT, () => {
-  console.log("Node Server Running");
+  console.log(
+    colors.bgBlue.white(`Node Server Running in ${process.env.DEV_MODE} mode PORT ${process.env.PORT}`)
+    );
 });
